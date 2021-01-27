1b52d787801c7422151d57aaa6678717a6a04d94
Arpita Mohapatra <64279181+Arpita-25@users.noreply.github.com>
2021-01-27 15:31:15 +0530

    Update original_gui.py

--- GUI/original_gui.py
+++ GUI/original_gui.py
@@ -9,7 +9,7 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
+    def __init__(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(800, 600)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
@@ -17,9 +17,11 @@ class Ui_MainWindow(object):
         self.label = QtWidgets.QLabel(self.centralwidget)
         self.label.setGeometry(QtCore.QRect(0, 50, 391, 411))
         self.label.setObjectName("label")
+        self.label.setScaledContents(True)
         self.label_2 = QtWidgets.QLabel(self.centralwidget)
         self.label_2.setGeometry(QtCore.QRect(400, 50, 391, 411))
         self.label_2.setObjectName("label_2")
+        self.label_2.setScaledContents(True)
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton.setGeometry(QtCore.QRect(312, 470, 161, 51))
         font = QtGui.QFont()
@@ -65,8 +67,8 @@ if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
+    ui = Ui_MainWindow(MainWindow)
+    #ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
 
